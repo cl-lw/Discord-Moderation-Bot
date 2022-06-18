a9ed4d385941df02c018abfbb8967d7ff45d996d
Wewwys <68690911+Mew-prog@users.noreply.github.com>
2022-06-18 23:08:51 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js");
 const ayarlar = require('../ayarlar.js');
 client.on("ready ")
 module.exports = async client => {
-  client.user.setPresence({ activity: { type: "WATCHING", name: `Wéwwys,BestKnife ❤ MoonArmy`}, status: 'dnd' })
+  client.user.setPresence({ activity: { type: "WATCHING", name: `Prefix: !!`}, status: 'dnd' })
 };
 
